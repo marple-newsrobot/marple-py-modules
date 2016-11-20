4c14776ac334d427858abcbd56e8cf4b88fddedb
jensfinnas <jens.finnas@gmail.com>
2016-11-20 18:57:51 +0100

    Add sample config

---
+++ tests/data/config.sample.py
@@ -0,0 +1,9 @@
+
+
+# For testing the postgrest module
+POSTGREST_URL = "https://marple-api.herokuapp.com"
+POSTGREST_TABLES = [
+    "dataset",
+]
+POSTGREST_ROLE = "name_of_db_user",
+POSTGREST_JWT_TOKEN = "long_and_hard_secret"
