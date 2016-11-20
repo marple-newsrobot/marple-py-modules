eb07d22b79c0db34b9b86f9a11e1444b5bbd2f60
jensfinnas <jens.finnas@gmail.com>
2016-11-20 19:55:08 +0100

    Add timepoint label function

---
+++ marple/utils.py
@@ -0,0 +1,43 @@
+# encoding: utf-8
+
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+def get_timepoint_label(datestring, periodicity):
+    """ Convert a datestring to a timepoint label.
+        :param datestring: an iso coded datestring. E.g. "2016-01-01"
+        :param periodicity: monthly|quarterly|yearly|rolling_quarter|rolling_year
+        :returns: a label for the timepoint
+        :rtype: str 
+    """
+    timepoint = datetime.strptime(datestring, '%Y-%m-%d')
+
+    if periodicity == "monthly":
+        # Jan 2016
+        return timepoint.strftime("%b %Y")
+
+    elif periodicity == "quarterly":
+        # Q1 2016
+        quarters = [0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4]
+        return "Q{} {}".format(
+            quarters[timepoint.month],
+            timepoint.year)
+
+    elif periodicity == "yearly":
+        # 2016
+        return str(timepoint.year)
+
+    elif periodicity == "rolling_quarter":
+        # 2016-03-01 => "Jan 2016-Mar 2016"
+        timepoint_start = timepoint - relativedelta(months=2)
+        return "{}-{}".format(
+            timepoint_start.strftime("%b %Y"),
+            timepoint.strftime("%b %Y"))
+
+    elif periodicity == "rolling_year":
+        # 2017-01-01 => "Feb 2016-Jan 2017"
+        timepoint_start = timepoint - relativedelta(months=11)
+        return "{}-{}".format(
+            timepoint_start.strftime("%b %Y"),
+            timepoint.strftime("%b %Y"),
+        )
