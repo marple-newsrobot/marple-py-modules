ac16c93851e4007712ebbb904f31a87f3ffae59d
jensfinnas <jens.finnas@gmail.com>
2016-12-02 10:50:33 +0100

    Get config from environment

--- tests/data/config.py
+++ tests/data/config.py
@@ -1,9 +1,11 @@
-
+# encoding: utf-8
+import os
 
 # For testing the postgrest module
 POSTGREST_URL = "https://marple-api.herokuapp.com"
 POSTGREST_TABLES = [
     "dataset",
 ]
-POSTGREST_ROLE = "name_of_db_user",
-POSTGREST_JWT_TOKEN = "long_and_hard_secret"
+POSTGREST_ROLE = os.environ.get('DB_USER')
+POSTGREST_JWT_TOKEN = os.environ.get('JWT_TOKEN')
+
